aba89527cc8d1cc6406a9268606a0148a3b98151
dip0011 <63140673+dip0011@users.noreply.github.com>
2021-06-05 12:44:57 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+$(".nav-btn").click(function(){
+  $("#menu").toggleClass("active");
+  $("#content").toggleClass("active");
+  $(".nav-btn").toggleClass("active");
+  $("h1").toggleClass("active");
+});
+// const menu = document.querySelector('#menu');
+// const content = document.querySelector('#content');
+// const navbtn = document.querySelector('.nav-btn');
+// const h1 = document.querySelector('h1');
+
+
+// function mytooglemenu(){
+//   menu.classList.toggle("active");
+// }
